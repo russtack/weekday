226ad27e6c91106aa178ce20c9c096a2d3e02fb6
russtack <stirlingest@gmail.com>
2023-04-08 09:58:27 -0400

    weekday

---
+++ WeekDay/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(GetDay(6));
+       
+    }
+    static string GetDay(int dayNum)
+    {
+        string dayName;
+        switch (dayNum)
+        {
+            case 0:
+                dayName = "Monday"; break;
+            case 1: dayName = "Tuesday"; break;
+            case 2: dayName = "Wednesday"; break;
+            case 3: dayName = "Thurday"; break;
+            case 4: dayName = "Friday"; break;
+            case 5: dayName = "Saturday";break;
+            case 6: dayName = "Sunday";break;
+            default: dayName = "invalid input"; break;
+        }
+        return dayName; 
+    }
+}
